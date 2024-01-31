b2fc58054e8331e949f8f98d85769f9cf4044f3f
Sanket Babar <53649051+sanketb892@users.noreply.github.com>
2024-01-31 19:19:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const productList = document.getElementById("product-list");
+
+const searchInput = document.getElementById("search-input");
+
+const searchBtn = document.getElementById("search-btn");
+
+const categoryBtn = document.querySelectorAll(".category-btn");
+
+
+
+function filterProduct(){
+const searchValue = searchInput.value.toLowerCase();
+const productItem = document.querySelectorAll(".product-item");
+const activeCategory = document.querySelector(".category-btn.active").dataset.category;
+
+productItem.forEach(item =>{
+    const title = item.querySelector("h3").innerText.toLowerCase();
+
+    const category = item.dataset.category;
+
+    if((title.includes(searchValue) ||searchValue=="") && 
+    (category === activeCategory || activeCategory ==="all")){
+        
+        item.style.display="block";
+    }else {
+        item.style.display="none";
+    } 
+})
+}
+
+function setCategory(e){
+    categoryBtn.forEach(btn=>btn.classList.remove("active"));
+        e.target.classList.add("active");
+        filterProduct();
+}
+
+searchBtn.addEventListener("click", filterProduct);
+searchInput.addEventListener("keyup", filterProduct);
+categoryBtn.forEach(btn=>{
+    btn.addEventListener("click", setCategory);
+})
+filterProduct();
